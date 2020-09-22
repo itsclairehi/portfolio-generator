06db697cf37ac7b03d4754d7fe72f962c0c971c9
claire p <cpuckett9@gmail.com>
2020-09-22 18:52:14 -0500

    working html generator

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length)
-console.log(profileDataArgs);
+//only works if line 7-8 are also in page-template.js... is this an instructional error? 9.2.6
+
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  };
-}
-  
-  printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
